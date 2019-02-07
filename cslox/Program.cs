75808feaa72d8d9bfe4f8c02b7779b8eccdb0057
Andrew Lee <and.rob.lee@gmail.com>
2019-02-06 18:56:47 -0800

    Add project files.

---
+++ cslox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cslox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
