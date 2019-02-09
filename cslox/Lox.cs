333da4ae21cc6d36a769806e0646c3126804b2b3
Andrew Lee <and.rob.lee@gmail.com>
2019-02-08 23:54:03 -0800

    Add recursive parser

--- cslox/Lox.cs
+++ cslox/Lox.cs
@@ -54,17 +54,29 @@ namespace cslox
         {
             Scanner scanner = new Scanner(source);
             List<Token> tokens = scanner.ScanTokens();
+            Parser parser = new Parser(tokens);
+            Expr expressions = parser.Parse();
 
-            foreach(Token token in tokens)
-            {
-                Console.WriteLine(token);
-            }
+            if (hadError) return;
 
+            Console.WriteLine(new ASTPrinter().Print(expressions));
         }
 
         public static void Error(int line, String message)
         {
             Report(line, "", message);
+        }
+
+        public static void Error(Token token, string message)
+        {
+            if (token.type == TokenType.EOF)
+            {
+                Report(token.line, " at end", message);
+            }
+            else
+            {
+                Report(token.line, " at '" + token.lexeme + "'", message);
+            }
         }
 
         private static void Report(int line, String where, String message)
---
+++ cslox/Parser.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cslox
+{
+    class Parser
+    {
+        private class ParseError: SystemException { }
+        private readonly List<Token> tokens;
+        private int current = 0;
+
+        public Parser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+        }
+
+        public Expr Parse()
+        {
+            try
+            {
+                return Expression();
+            }
+            catch (ParseError error)
+            {
+                return null;
+            }
+        }
+
+        private Expr Expression()
+        {
+            return Equality();
+        }
+
+        private Expr Equality()
+        {
+            Expr expr = Comparison();
+
+            while(Match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL))
+            {
+                Token op = Previous();
+                Expr right = Comparison();
+                expr = new Expr.Binary(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Comparison()
+        {
+            Expr expr = Addition();
+
+            while(Match(TokenType.GREATER, TokenType.GREATER_EQUAL, 
+                TokenType.LESS, TokenType.LESS_EQUAL))
+            {
+                Token op = Previous();
+                Expr right = Addition();
+                expr = new Expr.Binary(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Addition()
+        {
+            Expr expr = Multiplication();
+
+            while (Match(TokenType.MINUS, TokenType.PLUS))
+            {
+                Token op = Previous();
+                Expr right = Multiplication();
+                expr = new Expr.Binary(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Multiplication()
+        {
+            Expr expr = Unary();
+
+            while (Match(TokenType.SLASH, TokenType.STAR))
+            {
+                Token op = Previous();
+                Expr right = Unary();
+                expr = new Expr.Binary(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Unary()
+        {
+            if (Match(TokenType.BANG, TokenType.MINUS))
+            {
+                Token op = Previous();
+                Expr right = Unary();
+                return new Expr.Unary(op, right);
+            }
+
+            return Primary();
+        }
+
+        private Expr Primary()
+        {
+            if (Match(TokenType.FALSE)) return new Expr.Literal(false);
+            if (Match(TokenType.TRUE)) return new Expr.Literal(true);
+            if (Match(TokenType.NIL)) return new Expr.Literal(null);
+
+            if (Match(TokenType.NUMBER, TokenType.STRING))
+            {
+                return new Expr.Literal(Previous().literal);
+            }
+
+            if (Match(TokenType.LEFT_PAREN))
+            {
+                Expr expr = Expression();
+                Consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+                return new Expr.Grouping(expr);
+            }
+
+            throw Error(Peek(), "Expect expression.");
+        }
+
+        private Token Consume(TokenType type, string message)
+        {
+            if (Check(type)) return Advance();
+
+            throw Error(Peek(), message);
+        }
+
+        private bool Match(params TokenType[] types)
+        {
+            foreach (TokenType type in types)
+            {
+                if (Check(type))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool Check(TokenType type)
+        {
+            if (IsAtEnd()) return false;
+            return Peek().type == type;
+        }
+
+        private Token Advance()
+        {
+            if (!IsAtEnd()) current++;
+            return Previous();
+        }
+
+        private bool IsAtEnd()
+        {
+            return Peek().type == TokenType.EOF;
+        }
+
+        private Token Peek()
+        {
+            return tokens[current];
+        }
+
+        private Token Previous()
+        {
+            return tokens[current - 1];
+        }
+        private ParseError Error(Token token, string message)
+        {
+            Lox.Error(token, message);
+            return new ParseError();
+        }
+
+        private void Synchronize()
+        {
+            Advance();
+
+            while(!IsAtEnd())
+            {
+                if (Previous().type == TokenType.SEMICOLON) return;
+
+                switch(Peek().type)
+                {
+                    case TokenType.CLASS:
+                    case TokenType.FUN:
+                    case TokenType.VAR:
+                    case TokenType.FOR:
+                    case TokenType.IF:
+                    case TokenType.WHILE:
+                    case TokenType.PRINT:
+                    case TokenType.RETURN:
+                        return;
+                }
+
+                Advance();
+            }
+        }
+    }
+}
